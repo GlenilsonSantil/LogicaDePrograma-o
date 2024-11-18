578dde136c2b538e73e494229201a18a03fa65c1
Glenilson Santil <49415313+GlenilsonSantil@users.noreply.github.com>
2024-11-18 16:25:29 -0300

    Add files via upload

---
+++ app02.js
@@ -0,0 +1,20 @@
+let idade = 25;
+let possuiCarteira = true;
+
+// se idade é maior que 18 e possui carteira…
+if (idade > 18 && possuiCarteira) {
+  console.log("Pode dirigir!");
+} else {
+  console.log("Não pode dirigir.");
+}
+
+
+let temMaça = false;
+let temBanana = true;
+
+// se tem maça ou tem banana…
+if (temMaça || temBanana) {
+  console.log("Você tem frutas!");
+} else {
+  console.log("Não tem frutas.");
+}
